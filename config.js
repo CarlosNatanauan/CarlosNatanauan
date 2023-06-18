b47849deb22d9c3d80de94cf433379b6b270a52e
CarlosNatanauan <carlosbenedictn@gmail.com>
2023-06-18 11:20:02 +0800

    init

---
+++ config.js
@@ -0,0 +1,89 @@
+window.PorfolioConfig = { 
+  name: 'CARLOS',
+  jobTitle: 'Software Developer | Virtual Assistant',
+  jobDescription: 'Driven and passionate about technology, I have gained valuable experience through diverse academic projects. Eager to learn and grow professionally, I invite you to explore my showcased projects, which demonstrate my problem-solving and software development capabilities.',
+  
+  //end
+  skills: [
+    {
+      name: 'C# | Visual Studio',
+      description: 'Gained experience in developing various C# projects for school activities, including a Parking Management System for a thesis and a Stall Management System as a personal project.'
+    },
+    {
+      name: 'Java | Android Studio',
+      description: 'Familiar with Java-based Android app development, having tackled different logical activities in Android Studio for academic purposes.'
+    },
+    {
+      name: 'SQL | Microsoft SMS',
+      description: 'Acquired experience in Microsoft SQL, creating and managing databases for Visual Studio projects and academic activities, with a focus on efficient data organization and retrieval.'
+    },
+    {
+      name: 'SAP Business One | ERP Software',
+      description: 'Certified in SAP Business One Advanced (Implementation & Support), successfully completing a comprehensive training course to enhance business management and ERP skills.'
+    },
+    {
+      name: 'Networking Fundamentals',
+      description: 'Completed a HUAWEI-certified Network Foundations course and designed a network architecture for a barangay project, applying networking principles and technologies.'
+    },
+    {
+      name: 'SEO | Off-page',
+      description: 'One year of experience as a Virtual Assistant, specializing in Off-page SEO tasks to optimize website performance, increase online visibility, and achieve higher search engine rankings.'
+    }
+  ],
+  
+  //end
+  works: [
+    {
+      name: 'Parking Lot Management System',
+      link: 'https://jofftiquez.github.io/vue-stripe-checkout',
+      description: 'Developed an innovative Parking Management System for Metro Parking Philippines Inc., addressing their challenges with manual parking operations and revenue generation. Utilized C# .NET Visual Studio and Microsoft SQL Server Management to create a streamlined and efficient solution, enhancing the overall parking experience and optimizing revenue opportunities.'
+    },
+    {
+      name: 'Pair Game | Android',
+      link: 'https://theuselessweb.com/',
+      description: 'Developed a single-player memory card game for Android using Java in Android Studio, called Pair Game. Players match pairs of cards with the same image, sharpening their memory skills across three difficulty levels: Beginner (12 cards), Intermediate (20 cards), and Expert (20 cards with a time limit). The engaging gameplay offers a challenging and enjoyable experience for users.'
+    },
+    {
+      name: 'Stall Management System',
+      link: 'https://theuselessweb.com/',
+      description: 'Developed a comprehensive Stall Management System for Tagpuan Food Park using C# .NET Visual Studio and Microsoft SQL Server Management. The system enables users to manage stall availability, tenant information, set rental amounts, and print receipts. It also provides valuable insights into business analytics, streamlining operations and enhancing overall business management.'
+    }
+  ],
+  //end
+  hobbies: [
+    {
+      name: 'Rock Climbing',
+      description: 'Occaecat ipsum dolor ea ad ut irure do proident excepteur consectetur duis eu culpa velit.'
+    },
+    {
+      name: 'Surfing',
+      description: 'Laboris occaecat adipisicing culpa duis aute ea excepteur quis eiusmod mollit est pariatur.'
+    },
+    {
+      name: 'Rubick\'s Cube',
+      description: 'Laborum pariatur sint labore officia enim occaecat id.'
+    }
+  ],
+  //end
+  contacts: {
+    email: 'carlosbenedictn@gmail.com',
+    mobileNo: '+6399752720241',
+    links: [
+      {
+        name: 'Twitter',
+        link: 'https://twitter.com/NatanauanCarlos'
+      },
+      {
+        name: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/carlos-benedict-natanauan-981157235/'
+      },
+      {
+        name: 'GitHub',
+        link: 'https://github.com/CarlosNatanauan'
+      }
+    ]
+  }
+
+  
+};
+
